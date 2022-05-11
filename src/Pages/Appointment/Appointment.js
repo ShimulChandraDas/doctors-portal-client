3cf5f851d706ea567aa775ab1c1047e65ce5fed0
ShimulChandraDas <msshimul99@gmail.com>
2022-05-11 09:20:22 +0600

    book appointment desine done

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../Home/Shared/Footer';
 import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointments from './AvailableAppointments';
 
 
 
 const Appointment = () => {
+    const [date, setDate] = useState(new Date())
 
     return (
         <div  >
-            <AppointmentBanner />
+            <AppointmentBanner date={date} setDate={setDate} />
+            <AvailableAppointments date={date} />
             <Footer />
         </div>
     );
